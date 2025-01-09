a3c2bdcc424417eba8b81ee973faf9cc0ba02016
Sammienator <sammugo18@gmail.com>
2025-01-09 08:32:07 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About'
+import Projects from './components/Projects';
+import Quote from './components/Quote';
+import Footer from './components/Footer'
+
+
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
      <Header/> 
      <Hero/>
      <About/>
+     <Projects/>
+     <Quote/>
+     <Contact/>
+     <Footer/>
+
      
     </div>
   );
